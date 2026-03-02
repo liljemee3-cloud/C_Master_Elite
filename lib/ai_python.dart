de96d1356bcd805fa6ee97147cadededa816a29a
liljemee3-cloud <liljemee3@gmail.com>
2026-03-02 21:58:37 +0000

    Delete lib/ai_python.dart

--- lib/ai_python.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-class AIPythonScreen extends StatelessWidget {
-  const AIPythonScreen({super.key});
-  @override
-  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: Text("AI & Python")));
-}
-
