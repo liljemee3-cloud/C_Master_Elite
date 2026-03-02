935f086b43bca01967b2db7e942180477bbe6a78
liljemee3-cloud <liljemee3@gmail.com>
2026-03-02 23:04:55 +0000

    Create ai_python_module.dart

---
+++ lib/ai_python_module.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AIModule extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("بايثون والذكاء الاصطناعي"), backgroundColor: Colors.blueAccent),
+      body: Center(child: Text("شرح الشبكات العصبية وكلام آلان تورينج", style: TextStyle(color: Colors.white))),
+    );
+  }
+}
